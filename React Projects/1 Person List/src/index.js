66d3d78c01c24b67dda0fc66f7a1a657514ec013
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-01 19:15:27 +0200

    Person List DONE

--- React Projects/1 Person List/src/index.js
+++ React Projects/1 Person List/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '../src/index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Primer Component : Tarjeta con todos los datos
+const Person = ({ img, name, job, children }) => {
+  // TEMPLATE LITERALS NORMALES ---> ALOJADA EN VARIABLE // de JSX : {``}
+  const imgURL = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <article className='person'>
+      <img src={imgURL} alt='person' />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Function
+const PersonList = () => {
+  return (
+    <section className='person-list'>
+      <Person img='55' name='John' job='Dev'></Person>
+      <Person img='22' name='Bob' job='Fire'>
+        <p>I am a Children Text</p>
+      </Person>
+      <Person img='77' name='Dave' job='Design'></Person>
+    </section>
+  );
+};
+
+// Render
+ReactDOM.render(<PersonList></PersonList>, document.getElementById('root'));
